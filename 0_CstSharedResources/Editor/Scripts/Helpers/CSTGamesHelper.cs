53f09421aefc2d67d095c50819a1e7bf900225a8
Lam Van Quoc <lvq13302@gmail.com>
2025-10-18 19:08:25 +0700

    refactor(scripts): wrap CSTGamesHelper script into an editor namespace.

--- 0_CstSharedResources/Editor/Scripts/Helpers/CSTGamesHelper.cs
+++ 0_CstSharedResources/Editor/Scripts/Helpers/CSTGamesHelper.cs
@@ -1,21 +1,24 @@
 using UnityEditor;
 using UnityEngine;
 
-public static class CSTGamesHelper
-{
-	[MenuItem("CST Games/Player Prefs/Delete All Keys")]
-	public static void DeleteAllKeys()
+namespace CST.Shared.Resources.Editor
+{	
+	public static class CSTGamesHelper
 	{
-		if (EditorUtility.DisplayDialog("Delete All Player Prefs Keys", "Are you sure? These changes are irreversible by any means.",
-			"Yes, sir!", "Maybe not."))
+		[MenuItem("CST Games/Player Prefs/Delete All Keys")]
+		public static void DeleteAllKeys()
 		{
-			PlayerPrefs.DeleteAll();
+			if (EditorUtility.DisplayDialog("Delete All Player Prefs Keys", "Are you sure? These changes are irreversible by any means.",
+				"Yes, sir!", "Maybe not."))
+			{
+				PlayerPrefs.DeleteAll();
+			}
 		}
-	}
 
-	[MenuItem("CST Games/Data/Open Persistent Data Folder")]
-	public static void OpenPersistentDataFolder()
-	{
-		EditorUtility.RevealInFinder(Application.persistentDataPath);
+		[MenuItem("CST Games/Data/Open Persistent Data Folder")]
+		public static void OpenPersistentDataFolder()
+		{
+			EditorUtility.RevealInFinder(Application.persistentDataPath);
+		}
 	}
 }
