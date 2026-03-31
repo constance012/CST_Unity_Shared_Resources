b92c7188ebe44edab59bca4bb64de985d083a0ef
Lâm Văn Quốc <quoc.lv@mepplatform.com>
2026-03-31 16:40:21 +0700

    feat(windows-platform-drawer): add logic to draw platform build options for Windows.

--- 0_CstSharedResources/Editor/Scripts/Dashboard/Tabs/Build Handler/Platform Drawers/WindowsPlatformDrawer.cs
+++ 0_CstSharedResources/Editor/Scripts/Dashboard/Tabs/Build Handler/Platform Drawers/WindowsPlatformDrawer.cs
@@ -1,6 +1,9 @@
 using CSTGames.SharedResources.Editor.Dashboard.Interfaces;
 using CSTGames.SharedResources.Editor.Dashboard.Tabs.ProjectInfo;
+using CSTGames.SharedResources.Editor.Dashboard.Utilities;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
 
 namespace CSTGames.SharedResources.Editor.Dashboard.Tabs.BuildHandler
 {
@@ -10,26 +13,54 @@ namespace CSTGames.SharedResources.Editor.Dashboard.Tabs.BuildHandler
 		public string BuildTargetDisplayName => "Windows";
 
 		private ProjectInfoDataObject _projectInfoData;
+		private WindowsBuildArchitecture _buildArchitecture = WindowsBuildArchitecture.Intel64Bit;
+		private bool _shouldCopyPDBFiles = false;
+		private bool _shouldCreatVisualStudioSolution = false;
 
 		public WindowsPlatformDrawer(ProjectInfoDataObject projectInfoData)
 		{
 			_projectInfoData = projectInfoData;
-			// TO-DO: Custom constructor logic.
+
+			_buildArchitecture = _projectInfoData.TargetPlatform == BuildTarget.StandaloneWindows ? WindowsBuildArchitecture.Intel32Bit : WindowsBuildArchitecture.Intel64Bit;
 		}
 
 		public void Draw()
 		{
-			EditorGUILayout.HelpBox("Windows Platform Drawer content goes here...", MessageType.Info);
+			GUIDrawHelper.EnumPopupWithLabel("Build Architecture: ", ref _buildArchitecture, GUILayout.MinWidth(300f));
+
+			GUILayout.Space(10f);
+			GUIDrawHelper.ToggleWithLabel("Copy PDB Files: ", ref _shouldCopyPDBFiles, GUILayout.MinWidth(300f));
+			GUIDrawHelper.ToggleWithLabel("Create Visual Studio Solution: ", ref _shouldCreatVisualStudioSolution, GUILayout.MinWidth(300f));
 		}
 
 		public void SetupBuildParameters(string buildPath)
 		{
-			// TO-DO: Implement Windows build logic here.
+			UnityEditor.WindowsStandalone.UserBuildSettings.architecture = GetOsArchitecture();
+			UnityEditor.WindowsStandalone.UserBuildSettings.copyPDBFiles = _shouldCopyPDBFiles;
+			UnityEditor.WindowsStandalone.UserBuildSettings.createSolution = _shouldCreatVisualStudioSolution;
 		}
 
 		public string GetFileExtension()
 		{
 			return ".exe";
 		}
+
+		private OSArchitecture GetOsArchitecture()
+		{
+			return _buildArchitecture switch
+			{
+				WindowsBuildArchitecture.Intel64Bit => OSArchitecture.x64,
+				WindowsBuildArchitecture.Intel32Bit => OSArchitecture.x86,
+				WindowsBuildArchitecture.Arm64Bit => OSArchitecture.ARM64,
+				_ => OSArchitecture.x64
+			};
+		}
+	}
+
+	public enum WindowsBuildArchitecture
+	{
+		Intel64Bit = 0,
+		Intel32Bit = 1,
+		Arm64Bit = 2
 	}
 }
